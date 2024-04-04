878bf32c27661ebc014ea0cc7263e6960d3a0b59
Krushil Ramani <krushilramani34@gmail.com>
2024-04-04 15:27:23 +0530

    data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,12 +5,16 @@ from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
+import pickle
+import numpy as np
  
 load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
 password=os.getenv("password")
 db=os.getenv("db")
+
+
 
 def read_sql_data():
     logging.info("Reading sql databse started")
@@ -28,3 +32,15 @@ def read_sql_data():
         return df 
     except Exception as e:
         raise CustomException(e,sys)
+    
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
